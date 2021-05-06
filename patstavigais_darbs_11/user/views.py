6f839d0bb272c0c8f3f1c1cba6e8800ad161cfdb
demiurgs <81771907+demiurgs@users.noreply.github.com>
2021-05-06 10:31:25 +0300

    Update views.py
    class view, list view updated

--- patstavigais_darbs_11/user/views.py
+++ patstavigais_darbs_11/user/views.py
@@ -1,5 +1,5 @@
-#testing changes
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.views.generic import View, ListView
 from user.models import User
 
 
@@ -16,6 +16,47 @@ def index(request):
         request=request,
         context=context,
     )
+
+class VisitListView(ListView):
+    model = User
+    template_name='user_list.html'
+
+class AddUserView(View):
+
+    def get(self, request):
+
+        return render(
+            template_name='user.html',
+            request=request,
+            context=context,
+
+        )
+
+    def post(self, request):
+
+        if request.method == 'POST':
+            user = User(
+                username=request.POST['name'],
+                email=request.POST['email'],
+            )
+
+            user.save()
+
+            context = {
+                'user': user,
+            }
+
+            return render(
+                template_name='user.html',
+                request=request,
+                context=context,
+
+            )
+
+        return render(
+            template_name='form.html',
+            request=request
+        )
 
 
 def add_user(request):
@@ -61,29 +102,16 @@ def get_user(request, user_id):
 
     )
 
+class Delete_User(View):
 
-def delete_user(request, user_id):
+    def get(self, request):
+        return (f'Deleted {user.username}')
 
-    user = User.objects.get(pk=user_id)
-    user.delete()
-
-    return HttpResponse(f'Deleted {user.username}')
-
-
-def edit_user(request, user_id):
-
-    user = User.objects.get(id=user_id)
-
-    if request.method == 'POST':
-
-        username = request.POST['name']
-        email = request.POST['email']
-
-        if len(username) != 0:
-            user.username = username
-
-        if len(email) != 0:
-            user.email = email
+    def post(self, request):
+        user = User(
+            username=request.POST['name'],
+            email=request.POST['email'],
+        )
 
         user.save()
 
@@ -102,3 +130,45 @@ def edit_user(request, user_id):
         template_name='form.html',
         request=request
     )
+
+
+class Edit_User (View):
+
+    def get(self, request):
+        return render(
+            template_name='form.html',
+            request=request
+        )
+
+    def post(self, request):
+
+        user = User.objects.get(id=user_id)
+
+        if request.method == 'POST':
+
+            username = request.POST['name']
+            email = request.POST['email']
+
+            if len(username) != 0:
+                user.username = username
+
+            if len(email) != 0:
+                user.email = email
+
+            user.save()
+
+            context = {
+                'user': user,
+            }
+
+            return render(
+                template_name='user.html',
+                request=request,
+                context=context,
+
+            )
+
+        return render(
+            template_name='form.html',
+            request=request
+        )
